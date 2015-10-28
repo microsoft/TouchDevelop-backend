4bf18d5d99ffaa66a479a3e6539be800206d66bc
Michal Moskal <michal.moskal@microsoft.com>
2015-10-28 15:27:32 -0700

    Add mass settings import

--- src/tdlite-legacy.ts
+++ src/tdlite-legacy.ts
@@ -39,6 +39,7 @@ interface LegacySettings {
     TwitterHandle: string;
     Website: string;
     YearOfBirth: number;
+    FacebookId: string;
 }
 
 var legacyTable: azureTable.Client;
@@ -61,19 +62,33 @@ export async function initAsync()
         req.response = await importSettingsAsync(req.rootPub)
     });
    
+    core.addRoute("POST", "import", "usersettings", async(req: core.ApiRequest) => {
+        if (!core.checkPermission(req, "operator")) return;
+        let entities = await tdliteUsers.users.getIndex("all").fetchAsync("all", req.queryOptions);
+        let resp = {}
+        await parallel.forJsonAsync(entities.items, async(v) => {
+            let r = await importSettingsAsync(v);
+            resp[v["id"]] = r["code"];
+        }, 25);
+        req.response = {
+            continuation: entities.continuation,
+            publications: resp
+        };
+    });   
 }
 
 var normalFields = ["AboutMe", "Culture", "Email", "Gender", "HowFound", "Location", "Occupation",
                     "ProgrammingKnowledge", "RealName", "TwitterHandle", "Website"]
 
-async function importSettingsAsync(jsb: {})
-{    
-    let force = true;
-    
+async function importSettingsAsync(jsb: {}) {
+    let force = false;
+
     let s = await tdliteUsers.buildSettingsAsync(jsb);
     let s0 = JSON.stringify(s.toJson())
     let id = td.orEmpty(jsb["id"]);
     let res = await settingsTable.createQuery().partitionKeyIs(id).and("RowKey", "=", "$").fetchAllAsync()
+    let code = 200;
+    let loginid = "";
     if (res && res[0]) {
         let legacy = <LegacySettings>res[0];
         for (let k of normalFields) {
@@ -81,16 +96,25 @@ async function importSettingsAsync(jsb: {})
                 s[k.toLowerCase()] = legacy[k];
         }
         if (legacy.EditorMode) s.editorMode = legacy.EditorMode;
-        if (legacy.YearOfBirth) s.yearofbirth = legacy.YearOfBirth;                
+        if (legacy.YearOfBirth) s.yearofbirth = legacy.YearOfBirth;
+        if (legacy.FacebookId)
+            loginid = "fb:" + legacy.FacebookId;
+    } else {
+        code = 404;
     }
-    
+
     let s1 = JSON.stringify(s.toJson())
-    
-    if (force || s0 != s1) {
-        await tdliteUsers.users.reindexAsync(id, async(v) => {
-            tdliteUsers.applyUserSettings(v, s.toJson());
-        }, force)
-    }
-    
-    return s.toJson();
+
+
+    await tdliteUsers.users.reindexAsync(id, async(v) => {
+        tdliteUsers.applyUserSettings(v, s.toJson());
+        if (!v["permissions"])
+            v["permissions"] = ",user,";
+        if (loginid)
+            v["legacyLogin"] = loginid;
+    }, force)
+
+    let ret = s.toJson();
+    ret["code"] = code;
+    return ret;
 }
