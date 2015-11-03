d73fbc95dc5d521f7f1decf8491f68d4bdbeec1a
Michal Moskal <michal.moskal@microsoft.com>
2015-11-03 11:56:06 -0800

    Support zip art

--- src/tdlite-data.ts
+++ src/tdlite-data.ts
@@ -106,6 +106,7 @@ export var artContentTypes: td.SMap<string> =
       "text/plain": "txt",
       "application/pdf": "pdf",
       "video/mp4": "mp4",
+      "application/x-zip-compressed": "zip",
       "application/vnd.openxmlformats-officedocument.wordprocessingml.document": "docx",
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": "xlsx",
       "application/vnd.openxmlformats-officedocument.presentationml.presentation": "pptx"
