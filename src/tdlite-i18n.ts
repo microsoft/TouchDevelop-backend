6197503132cead6e7e3b03528cddddde75b7c92c
Michal Moskal <michal.moskal@microsoft.com>
2016-05-04 15:41:04 +0100

    Format

--- src/tdlite-i18n.ts
+++ src/tdlite-i18n.ts
@@ -13,10 +13,9 @@ import * as cachedStore from "./cached-store"
 
 var logger = core.logger;
 var httpCode = core.httpCode;
-var i18nCache:cachedStore.Container;
+var i18nCache: cachedStore.Container;
 
-async function downloadCachedTranslationAsync(filename:string, lang:string)
-{
+async function downloadCachedTranslationAsync(filename: string, lang: string) {
 	let key = "i18n:" + filename + ":" + lang
 	let f = await core.redisClient.getAsync(key)
 	if (f)
@@ -24,12 +23,11 @@ async function downloadCachedTranslationAsync(filename:string, lang:string)
 	let dat = await crowdin.downloadTranslationAsync(filename, lang)
 	dat = dat || {}
 	logger.info(`crowdin dl: ${key} - ${Object.keys(dat).length}`)
-	await core.redisClient.setpxAsync(key, JSON.stringify(dat), 10*60*1000)
+	await core.redisClient.setpxAsync(key, JSON.stringify(dat), 10 * 60 * 1000)
 	return dat
 }
 
-export async function translateHtmlAsync(html:string, lang:string[])
-{
+export async function translateHtmlAsync(html: string, lang: string[]) {
 	//logger.debug(`crowdin translate html: ${crowdin.enabled} '${lang[0]}' ${html.length}`)
 	if (!crowdin.enabled || !lang || !lang[0]) return html;
 	let trdata = await downloadCachedTranslationAsync("website.json", lang[0])
@@ -41,20 +39,20 @@ export async function initAsync() {
 	if (core.hasSetting("CROWDIN_KEY"))
 		crowdin.init();
 	else return;
-	
-	core.addRoute("POST", "i18n", "upload", async(req) => {
+
+	core.addRoute("POST", "i18n", "upload", async (req) => {
 		if (!core.checkPermission(req, "i18n"))
 			return;
 		let resp = await crowdin.uploadTranslationAsync(td.toString(req.body["filename"]), req.body["strings"])
 		req.response = {};
 	})
 
-	core.addRoute("POST", "i18n", "pointers", async(req) => {
+	core.addRoute("POST", "i18n", "pointers", async (req) => {
 		if (!core.checkPermission(req, "i18n"))
 			return;
 		let strs = td.toStringArray(req.body["pointers"])
 		let res = {}
-		await parallel.forJsonAsync(strs, async(fn) => {
+		await parallel.forJsonAsync(strs, async (fn) => {
 			let ptr = await core.getPubAsync(fn, "pointer")
 			if (ptr && ptr["pub"]["htmlartid"]) {
 				let text = await tdlitePointers.getTemplateTextAsync(fn.replace(/^ptr-/, ""), [])
@@ -64,14 +62,14 @@ export async function initAsync() {
 		let phrases = Object.keys(res)
 		phrases.sort(td.strcmp)
 		let obj = td.toDictionary(phrases, s => s)
-		await crowdin.uploadTranslationAsync("website.json", obj)		
+		await crowdin.uploadTranslationAsync("website.json", obj)
 		req.response = {
 			pointers: strs,
 			phrases: phrases
 		}
 	});
 
-	core.addRoute("GET", "*pointer", "i18n", async(req) => {
+	core.addRoute("GET", "*pointer", "i18n", async (req) => {
 		let text = await tdlitePointers.getTemplateTextAsync(req.rootId.replace(/^ptr-/, ""), [])
 		req.response = crowdin.translate(text, {}).missing;
 	})
