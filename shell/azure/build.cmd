56cd2746bacf4184e6c25346a34e25525dc93310
Peli de Halleux <jhalleux@microsoft.com>
2016-10-04 13:16:34 -0700

    adding pre-build step

--- shell/azure/build.cmd
+++ shell/azure/build.cmd
@@ -2,6 +2,10 @@
 
 SET THIS=%~dp0
 
+cd ..
+node ../node_modules/typescript/bin/tsc
+cd azure
+
 "C:\Program Files\Microsoft SDKs\Azure\.NET SDK\v2.9\bin\cspack.exe" %THIS%\tdshell.csdef /out:%THIS%\..\..\built\tdshell.cspkg /roleFiles:ShellRole;%THIS%\files.txt
 if %ERRORLEVEL% NEQ 0 (
     echo Error building bootstrap.cspkg. Make sure cspack.exe from Windows Azure SDK is on the PATH.
